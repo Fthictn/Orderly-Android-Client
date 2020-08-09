af706e59e858f421bd42fa1b2c50f27393ae4246
Fthictn <fthi.cetin@gmail.com>
2020-08-09 05:48:02 +0300

    create project and add question added

--- app/src/main/java/com/fethicectin/orderly/Activities/AddQuestionActivity.kt
+++ app/src/main/java/com/fethicectin/orderly/Activities/AddQuestionActivity.kt
@@ -1,13 +1,22 @@
 package com.fethicectin.orderly.Activities
 
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.fethicectin.orderly.R
+
 
 class AddQuestionActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_question)
+    }
+
+    fun addQuestionOnClick(view: View) {}
+    fun cancelOnClick(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/fethicectin/orderly/Activities/CreateNewProjectActivity.kt
+++ app/src/main/java/com/fethicectin/orderly/Activities/CreateNewProjectActivity.kt
@@ -1,13 +1,22 @@
 package com.fethicectin.orderly.Activities
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.fethicectin.orderly.R
 
 class CreateNewProjectActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_new_project)
+    }
+
+    fun createProjectOnClick(view: View) {}
+    fun cancelProjectOnClick(view: View) {
+        val intent = Intent(this, ProjectActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
